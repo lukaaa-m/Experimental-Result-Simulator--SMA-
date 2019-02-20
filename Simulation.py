cfd99f02cde9e5012705a1b820f388122e50e7c0
lukaaa-m <lukablueowl51@gmail.com>
2019-02-20 17:42:19 +1000

    Game Design Implemented
    Based off application of the pigeonhole theorem to digital sums, where a random number of three digit numbers are generated, and that number is divided by the highest possible number of unique digital sums derived from a three digit number. The floor of this operation is one output (the maximum repeats for every possible digital sum) and the remainder is the other output (the maximum number of digital sums that are repeated more than the others). Conditions for successful outcomes are still to be determined (maybe based on even and odd combinations of common- and lone- repeats?).

--- Simulation.py
+++ Simulation.py
@@ -1,15 +1,27 @@
-import pandas as pd #Excel writing and reading package
+import pandas as pd,math,random
 
-experiment_count = 1000000 #Number of experimental iterations
-experiment_results = [] #List of all experimental results
+experiment_count = 5 #Number of experimental iterations
+common_repeats_results = [] #List of common repeats
+lone_repeats_results = [] #List of maximum lone repeats
+digits = 3
+total_digit_sums = 9 * digits
 
 #Starts with random input, enforces game rules to give final output
 for i in range(experiment_count):
-    #Do game rule stuff
-    result = 0 #Final output after game rule enforcement
-    experiment_results.append(result)
+    num_count = random.randint(1,161)
 
-#Compiles results into dataframe to be written to Excel spreadsheet
-results_dataframe = pd.DataFrame(data=experiment_results, index=None,                               columns="Results", dtype=None, copy=False)
+    common_repeats = math.floor(num_count / total_digit_sums)
+    lone_repeats = num_count % total_digit_sums
+
+    common_repeats_results.append(common_repeats)
+    lone_repeats_results.append(lone_repeats)
+
+    print("Numbers =", num_count)
+    print (common_repeats_results[i], ":", lone_repeats_results[i])
+
+
+#Compiles results into two-column dataframe to be written to Excel spreadsheet
+########
 
 #Write dataframe to excel
+########
